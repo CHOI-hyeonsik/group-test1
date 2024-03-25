6923c5d9fe6fa3dab7e86ef6064ef25c6472820d
chs8003 <chsshc_@naver.com>
2024-03-25 21:57:23 +0900

    first commit

---
+++ src/test/java/com/hyeonsik/boot/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyeonsik.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
